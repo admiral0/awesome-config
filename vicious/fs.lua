08449e800089919f96d598a9112ce853cd6590b2
Radu Andries <admiral0@tuxfamily.org>
2010-03-06 01:43:14 +0100

    Changed vicious so it is a separate repo

---
+++ vicious/fs.lua
@@ -0,0 +1,49 @@
+---------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2010, Adrian C. <anrxc@sysphere.org>
+--  * (c) 2009, Lucas de Vries <lucas@glacicle.com>
+---------------------------------------------------
+
+-- {{{ Grab environment
+local tonumber = tonumber
+local io = { popen = io.popen }
+local setmetatable = setmetatable
+local string = { match = string.match }
+local helpers = require("vicious.helpers")
+-- }}}
+
+
+-- FS: provides file system disk space usage
+module("vicious.fs")
+
+
+-- Variable definitions
+local unit = { ["mb"] = 1024, ["gb"] = 1024^2 }
+
+-- {{{ Filesystem widget type
+local function worker(format, nfs)
+    -- Fallback to listing only local file systems
+    if nfs then nfs = "" else nfs = "--local" end
+
+    -- Get data from df
+    local f = io.popen("LANG=C df -kP " .. nfs)
+    local fs_info = {}
+
+    for line in f:lines() do
+        if not string.match(line, "^Filesystem.*") then
+            local s, u, a, p, m = string.match(line, -- Match all at once (including NFS)
+             "^[%w%p]+[%s]+([%d]+)[%s]+([%d]+)[%s]+([%d]+)[%s]+([%d]+)%%[%s]+([%w%p]+)$")
+
+            helpers.uformat(fs_info, m .. " size",  s, unit)
+            helpers.uformat(fs_info, m .. " used",  u, unit)
+            helpers.uformat(fs_info, m .. " avail", a, unit)
+            fs_info["{" .. m .. " used_p}"] = tonumber(p)
+        end
+    end
+    f:close()
+
+    return fs_info
+end
+-- }}}
+
+setmetatable(_M, { __call = function(_, ...) return worker(...) end })
---
+++ vicious/helpers.lua
@@ -0,0 +1,122 @@
+---------------------------------------------------
+-- Licensed under the GNU General Public License v2
+--  * (c) 2010, Adrian C. <anrxc@sysphere.org>
+--  * (c) 2009, Rémy C. <shikamaru@mandriva.org>
+--  * (c) 2009, Benedikt Sauer <filmor@gmail.com>
+--  * (c) 2009, Henning Glawe <glaweh@debian.org>
+--  * (c) 2009, Lucas de Vries <lucas@glacicle.com>
+---------------------------------------------------
+
+-- {{{ Grab environment
+local pairs = pairs
+local io = { open = io.open }
+local setmetatable = setmetatable
+local string = {
+    sub = string.sub,
+    gsub = string.gsub,
+    format = string.format
+}
+-- }}}
+
+
+-- Helpers: provides helper functions for vicious widgets
+module("vicious.helpers")
+
+
+-- {{{ Variable definitions
+local scroller = {}
+-- }}}
+
+-- {{{ Helper functions
+-- {{{ Expose path as a Lua table
+function pathtotable(path)
+    return setmetatable({},
+        { __index = function(_, name)
+            local f = io.open(path .. '/' .. name)
+            if f then
+                local s = f:read("*all")
+                f:close()
+                return s
+            end
+        end
+    })
+end
+-- }}}
+
+-- {{{ Format a string with args
+function format(format, args)
+    for var, val in pairs(args) do
+        format = string.gsub(format, "$" .. var, val)
+    end
+
+    return format
+end
+-- }}}
+
+-- {{{ Format units to one decimal point
+function uformat(array, key, value, unit)
+    for u, v in pairs(unit) do
+        array["{"..key.."_"..u.."}"] = string.format("%.1f", value/v)
+    end
+
+    return array
+end
+-- }}}
+
+-- {{{ Escape a string
+function escape(text)
+    local xml_entities = {
+        ["\""] = "&quot;",
+        ["&"]  = "&amp;",
+        ["'"]  = "&apos;",
+        ["<"]  = "&lt;",
+        [">"]  = "&gt;"
+    }
+
+    return text and text:gsub("[\"&'<>]", xml_entities)
+end
+-- }}}
+
+-- {{{ Truncate a string
+function truncate(text, maxlen)
+    local txtlen = text:len()
+
+    if txtlen > maxlen then
+        text = string.sub(text, 1, maxlen - 3) .. "..."
+    end
+
+    return text
+end
+-- }}}
+
+-- {{{ Scroll through a string
+function scroll(text, maxlen, widget)
+    if not scroller[widget] then
+        scroller[widget] = { i = 1, d = true }
+    end
+
+    local txtlen = text:len()
+    local state  = scroller[widget]
+
+    if txtlen > maxlen then
+        if state.d then
+            text = string.sub(text, state.i, state.i + maxlen) .. "..."
+            state.i = state.i + 3
+
+            if maxlen + state.i >= txtlen then
+                state.d = false
+            end
+        else
+            text = "..." .. string.sub(text, state.i, state.i + maxlen)
+            state.i = state.i - 3
+
+            if state.i <= 1 then
+                state.d = true
+            end
+        end
+    end
+
+    return text
+end
+-- }}}
+-- }}}
