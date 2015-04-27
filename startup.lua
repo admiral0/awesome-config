98ce72f9a639202a1c5ab298bd505bc6518e4831
Radu Andries <radu.andries@tellnet.it>
2015-04-27 16:45:33 +0200

    Start from 0

--- startup.lua
+++
@@ -1,6 +0,0 @@
--- awful.util.spawn("nm-applet --sm-disable")
-awful.util.spawn("xcompmgr")
--- -- avoid Java Fun
-awful.util.spawn("wmname LG3D")
-awful.util.spawn("start-pulseaudio-x11")
-awful.util.spawn("parcellite")
